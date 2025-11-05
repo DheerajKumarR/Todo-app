71e9c00166c1ee94a78654988b0c22ad5deaa22e
DheerajKumarR <dheerajkumarrdkr@gmail.com>
2025-11-05 23:51:52 +0530

    Connected to Railway DB

--- src/main/java/com/todo/util/DatabaseConnection.java
+++ src/main/java/com/todo/util/DatabaseConnection.java
@@ -8,22 +8,23 @@ public class DatabaseConnection {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    private static final String HOST = System.getenv("DB_HOST");
-    private static final String PORT = System.getenv("DB_PORT");
-    private static final String DB = System.getenv("DB_NAME");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
-
-    private static final String URL = 
-        "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB +
-        "?sslMode=REQUIRED&allowPublicKeyRetrieval=true&autoReconnect=true";
-
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName(DRIVER);
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+            String host = System.getenv("DB_HOST");
+            String port = System.getenv("DB_PORT");
+            String db   = System.getenv("DB_NAME");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASSWORD");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + db +
+                         "?useSSL=true&requireSSL=false&autoReconnect=true&allowPublicKeyRetrieval=true";
+
+            return DriverManager.getConnection(url, user, pass);
+
         } catch (ClassNotFoundException e) {
-            throw new SQLException("MySQL JDBC Driver not found", e);
+            throw new SQLException("MySQL Driver not found", e);
         }
     }
 }
